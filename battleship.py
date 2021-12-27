c521a6f1af4d34748abb2391bf94a1a3441e59d6
Madhu-Lumiseek <madhu.k@lumiseek.com>
2021-12-27 17:22:55 +0530

    completed is vertical implementation

--- battleship.py
+++ battleship.py
@@ -28,13 +28,17 @@ Returns: None
 def makeModel(data):
     # data={}
     data["rows"]=10
+    # print("data",data)
     data["cols"]=10
     data["boardsize"]=500
     data["cellsize"]=data["boardsize"]/data["rows"]
-    data["userboard"]=test.testGrid()
+    data["userboard"]=emptyGrid(data["rows"],data["cols"])
     data["computerboard"]=emptyGrid(data["rows"],data["cols"])
     data["numberofships"] = 5
     data["computerboard"]=addShips(data["computerboard"] ,data["numberofships"])
+    data["tempship"]=[]
+    data["userShip"]=0
+    print("data",data)
     return data
 
 # print(makeModel({}))
@@ -49,6 +53,7 @@ Returns: None
 def makeView(data, userCanvas, compCanvas):
     drawGrid(data, userCanvas,data["userboard"],True)
     drawGrid(data,compCanvas,data["computerboard"],False)
+    drawShip(data, userCanvas, data["tempship"])
     return
 
 
@@ -67,6 +72,9 @@ Parameters: dict mapping strs to values ; mouse event object ; 2D list of ints
 Returns: None
 '''
 def mousePressed(data, event, board):
+    ccell = getClickedCell(data, event)
+    if board == "user":
+        clickUserBoard(data, ccell[0], ccell[1])
     pass
 
 #### WEEK 1 ####
@@ -82,7 +90,9 @@ def emptyGrid(rows, cols):
         value=[]
         for j in range(cols):
             value.append(EMPTY_UNCLICKED)
+        # print("value",value)
         grid.append(value)
+    # print("grid",grid)    
     return grid
 
 
@@ -97,8 +107,10 @@ def createShip():
     shipOrientation=random.randint(0,1)
     if shipOrientation == 0:
         ship = [[r-1,c],[r,c],[r+1,c]]
+        # print("ship0",ship)
     else:
         ship = [[r,c-1],[r,c],[r,c+1]]
+    print("ship",ship)
     return ship
 
 
@@ -130,7 +142,7 @@ def addShips(grid, numShips):
             for y in newship:
                 grid[y[0]][y[1]]=SHIP_UNCLICKED
             count+=1
-       
+    # print("shipsadded",grid)
     return grid
 
 
@@ -143,11 +155,14 @@ def drawGrid(data, canvas, grid, showShips):
     for row in range(data["rows"]):
         for cols in range(data["cols"]):
             if grid[row][cols] == SHIP_UNCLICKED:
-                canvas.create_rectangle(cols*data["cellsize"],row*data["cellsize"],data["cellsize"]+cols*data["cellsize"], row*data["cellsize"]+data["cellsize"], fill="yellow")
+                canvas.create_rectangle(cols*data["cellsize"],row*data["cellsize"],data["cellsize"]+cols*data["cellsize"], 
+                row*data["cellsize"]+data["cellsize"], fill="yellow")
             else:
-                canvas.create_rectangle(cols*data["cellsize"],row*data["cellsize"],data["cellsize"]+cols*data["cellsize"], row*data["cellsize"]+data["cellsize"], fill="blue")
+                canvas.create_rectangle(cols*data["cellsize"],row*data["cellsize"],data["cellsize"]+cols*data["cellsize"], 
+                row*data["cellsize"]+data["cellsize"], fill="blue")
         else:
-            canvas.create_rectangle(cols*data["cellsize"],row*data["cellsize"],data["cellsize"]+cols*data["cellsize"], row*data["cellsize"]+data["cellsize"], fill="blue")
+            canvas.create_rectangle(cols*data["cellsize"],row*data["cellsize"],data["cellsize"]+cols*data["cellsize"], 
+            row*data["cellsize"]+data["cellsize"], fill="blue")
     return
 
 
@@ -159,8 +174,17 @@ Parameters: 2D list of ints
 Returns: bool
 '''
 def isVertical(ship):
-    return
-
+    # print("vertical",ship)
+    i=0 
+    if ship[i][1]==ship[i+1][1]==ship[i+2][1]: 
+        ship.sort() 
+        if ship[i+1][0]-ship[i][0]==1 and ship[i+2][0]-ship[i+1][0]==1:
+            # print("vertical",ship)
+            return True 
+    return False
+# assert(isVertical([ [2, 1], [0, 1], [1, 1] ]) == True)
+# assert(isVertical([ [1, 0], [3, 2], [0, 1] ]) == False)
+# assert(isVertical([ [4, 5], [5, 5], [6, 5] ]) == True)
 
 '''
 isHorizontal(ship)
@@ -168,7 +192,13 @@ Parameters: 2D list of ints
 Returns: bool
 '''
 def isHorizontal(ship):
-    return
+    i=0 
+    if ship[i][0]==ship[i+1][0]==ship[i+2][0]: 
+        ship.sort() 
+        if ship[i+1][1]-ship[i][1]==1 and ship[i+2][1]-ship[i+1][1]==1:
+            # print("horizontal",ship)
+            return True 
+    return False
 
 
 '''
@@ -177,7 +207,14 @@ Parameters: dict mapping strs to values ; mouse event object
 Returns: list of ints
 '''
 def getClickedCell(data, event):
-    return
+    csize=int(data["cellsize"])
+    x_cordinate = event.x
+    print("x",event.x)
+    y_cordinate = event.y
+    print("y",event.y)
+    data["getClickedCell"] = [int(event.y/csize), int(event.x/csize)]
+    # print(data["getClickedCell"])
+    return data["getClickedCell"]
 
 
 '''
@@ -186,6 +223,11 @@ Parameters: dict mapping strs to values ; Tkinter canvas; 2D list of ints
 Returns: None
 '''
 def drawShip(data, canvas, ship):
+    for a in ship:
+        fst=a[0]
+        sec=a[1]
+        canvas.create_rectangle(sec*data["cellsize"],fst * data["cellsize"], (1 + sec) * data["cellsize"], 
+                                       (fst + 1)*data["cellsize"],fill="white")
     return
 
 
@@ -195,7 +237,14 @@ Parameters: 2D list of ints ; 2D list of ints
 Returns: bool
 '''
 def shipIsValid(grid, ship):
-    return
+    v = isVertical(ship)
+    h = isHorizontal(ship)
+    cship=checkShip(grid, ship)
+    if cship == True:
+        if len(ship) == 3:
+            if v == True or h == True:
+                return True
+    return False
 
 
 '''
@@ -204,6 +253,14 @@ Parameters: dict mapping strs to values
 Returns: None
 '''
 def placeShip(data):
+    egrid = data["userboard"]
+    if shipIsValid(egrid, data["tempship"]) == True:
+        for x in data["tempship"]:
+            egrid[x[0]][x[1]] = SHIP_UNCLICKED
+        data["userShip"] = data["userShip"] + 1
+    else:
+        print("ship is not valid")
+    data["tempship"] = []
     return
 
 
@@ -213,6 +270,14 @@ Parameters: dict mapping strs to values ; int ; int
 Returns: None
 '''
 def clickUserBoard(data, row, col):
+    rc=data["userboard"]
+    if [row,col] in rc or data["userShip"] == 5:
+        return
+    data["tempship"].append([row,col])
+    if len(data["tempship"]) == 3:
+        placeShip(data)
+    if data["userShip"] == 5:
+        print("you can start the game")
     return
 
 
@@ -301,7 +366,7 @@ def runSimulation(w, h):
     compCanvas = Canvas(compWindow, width=w, height=h)
     compCanvas.configure(bd=0, highlightthickness=0)
     compCanvas.pack()
-
+    # print("canvas",compCanvas,"33333333")
     makeView(data, userCanvas, compCanvas)
 
     root.bind("<Key>", lambda event : keyEventHandler(data, userCanvas, compCanvas, event))
@@ -318,9 +383,9 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testDrawGrid()
-    test.testMakeModel()
-    test.testGrid()
+    
+    
+    
 
     ## Finally, run the simulation to test it manually ##
     runSimulation(500, 500)
--- codes.py
+++ codes.py
@@ -1,10 +1,13 @@
-# d = { "apples" : 5, "beets" : 2, "lemons" : 1 }
-# keys=d.keys()
-# values=d.values()
-# # print(keys)
-# # print(values)
-# for i in d:
-#     print(d[i],i)
+d = { "apples" : 5, "beets" : 2, "lemons" : 1 }
+keys=d.keys()
+values=d.values()
+# print("keys:" ,keys)
+# print(values)
+temp = 1
+for i in d:
+    temp = temp * d[i]
+    print(d[i])
+print(temp)
 # print(sum(values))
 
 
@@ -28,23 +31,23 @@
 # print(mostCommonFirstLetter("do you have a voting plan for the election happening next month?"))
 
 
-import tkinter as tk
-def draw(canvas):
-    pass
-def makeCanvas(w, h):
-    root=tk.Tk()
-    canvas=tk.Canvas(root,width=w,height=h)
-    canvas.configure(bd=0,highlightthicknes=0)
-    # canvas.create_rectangle(40,40,80,140,width=5,fill="red")
-    # canvas.create_oval(30, 80, 150, 200,width=20, fill="#FF69B4")
-    # canvas.create_rectangle(90, 70, 180, 120,width=0, fill="palegreen")
-    # canvas.create_line(200, 300, 400, 350)
-    # canvas.create_line(20, 100, 90, 300, fill="green")
-    # canvas.create_line(100, 100, 300, 300, width=5)
-    canvas.create_text(200, 200, text="Hello World!", font="Arial") 
-    canvas.create_text(100, 100, text="This is fun!", font="Times 30") 
-    canvas.create_text(300, 300, text="weewooweewoo", font="Courier 10 italic")
-    canvas.pack()
-    draw(canvas)
-    root.mainloop()
-makeCanvas(400,400)
+# import tkinter as tk
+# def draw(canvas):
+#     pass
+# def makeCanvas(w, h):
+#     root=tk.Tk()
+#     canvas=tk.Canvas(root,width=w,height=h)
+#     canvas.configure(bd=0,highlightthicknes=0)
+#     # canvas.create_rectangle(40,40,80,140,width=5,fill="red")
+#     # canvas.create_oval(30, 80, 150, 200,width=20, fill="#FF69B4")
+#     # canvas.create_rectangle(90, 70, 180, 120,width=0, fill="palegreen")
+#     # canvas.create_line(200, 300, 400, 350)
+#     # canvas.create_line(20, 100, 90, 300, fill="green")
+#     # canvas.create_line(100, 100, 300, 300, width=5)
+#     canvas.create_text(200, 200, text="Hello World!", font="Arial") 
+#     canvas.create_text(100, 100, text="This is fun!", font="Times 30") 
+#     canvas.create_text(300, 300, text="weewooweewoo", font="Courier 10 italic")
+#     canvas.pack()
+#     draw(canvas)
+#     root.mainloop()
+# makeCanvas(400,400)
